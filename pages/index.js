88b7d1b9d4fc557db6f7ab2a32e626d37b9f0154
Abu Hurera <abu.hurera@venturenox.com>
2023-08-03 15:47:22 +0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,205 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+
+    <div className='image-container'>
+    <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={100}
+        height={24}
+      />
+      <Image
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (1).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (2).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (3).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+    </div>
+    <div className='image-container'>
+    <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={100}
+        height={24}
+      />
+      <Image
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (1).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (2).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (3).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+    </div>
+    <div className='image-container'>
+    <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={100}
+        height={24}
+      />
+      <Image
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (1).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (2).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (3).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+    </div>
+    <div className='image-container'>
+    <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={100}
+        height={24}
+      />
+      <Image
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (1).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (2).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (3).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+    </div>
+    <div className='image-container'>
+    <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={100}
+        height={24}
+      />
+      <Image
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (1).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (2).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com (3).svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+      <Image
+        src="/web-site-svgrepo-com.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+      />
+    </div>
+
+
+    </>
+  )
+}
