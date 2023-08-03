2c5977eb5dea7c9c9f0599791b600f2ad4ad6fdf
Abu Hurera <abu.hurera@venturenox.com>
2023-08-03 22:41:51 +0500

    used blur property

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,40 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useState, useEffect } from 'react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const [base64Image, setBase64Image] = useState(null);
+
+
+  useEffect(() => {
+    // Fetch the image and convert it to Base64
+    fetch('/logo.png')
+      .then((response) => response.blob())
+      .then((blob) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setBase64Image(reader.result);
+        };
+        reader.readAsDataURL(blob);
+      })
+      .catch((error) => console.error('Error fetching image:', error));
+  }, []);
   return (
     <>
 
     <div className='image-container'>
-    <Image
-        src="/1690525371exotic-beach-in-the-summer.svg"
-        alt="Vercel Logo"
-        width={100}
-        height={24}
-      />
-      <Image
-        src="/1690525371exotic-beach-in-the-summer.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-      />
       <Image
         src="/trail-5yOnGsKUNGw-unsplash.jpg"
-        blurDataURL='/notebook-svgrepo-com.svg'
+        blurDataURL='/trail-5yOnGsKUNGw-unsplash_11zon.jpg'
         placeholder='blur'
         alt="Next.js Logo"
         width={400}
         height={400}
-      />
-      <Image
-        src="/1690525371exotic-beach-in-the-summer.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-      />
-      <Image
-        src="/1690525371exotic-beach-in-the-summer.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-      />
-      <Image
-        src="/1690525371exotic-beach-in-the-summer.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
       />
     </div>
 
