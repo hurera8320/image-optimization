9e23480025a621da007d2ff914f7ae704b8705cf
Abu Hurera <abu.hurera@venturenox.com>
2023-08-04 14:16:13 +0500

    testing

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import image from "../public/masahiro-miyagi-xk0YHAn3dzk-unsplash.jpg";
 import images from "../public/images";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
 
-  const [imageLoading , setImageLoading] = useState(false)
+  const [imageLoading, setImageLoading] = useState(false)
+
+  useEffect(() => {
+    console.log("===>", imageLoading)
+  }, [imageLoading])
 
   return (
     <div className={styles.container}>
@@ -17,8 +21,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.body}>
-        <section className={styles.section}>
         <h1>Unoptimized Images</h1>
+        <section className={styles.section}>
           <figure>
             <div className={styles.imageWrapper}>
               <Image
@@ -43,8 +47,8 @@ export default function Home() {
           </figure>
 
         </section>
-        <section className={styles.section}>
         <h1>optimized Images using blurDataUrl and placeholder</h1>
+        <section className={styles.section}>
           {images.map((image, index) => (
             <figure key={index}>
               <div className={styles.imageWrapper}>
@@ -52,7 +56,7 @@ export default function Home() {
                   className={styles.image}
                   src={image.src}
                   alt={image.src}
-                  priority={index%2 === 0 ? true : false}
+                  priority={index % 2 === 0 ? true : false}
                   placeholder="blur"
                   blurDataURL={image.blurUrl}
                   layout="fill"
@@ -60,31 +64,35 @@ export default function Home() {
               </div>
               <figcaption className={styles.imageCaption}>
                 Image {index + 3} is stored remotely and uses a placeholder.
-                <h3>{index%2 === 0 && "Priority Image"} </h3>
+                <h3>{index % 2 === 0 && "Priority Image"} </h3>
               </figcaption>
             </figure>
           ))} </section>
 
-          <section>
-            <h1> Using low quality Image as placeholder </h1>
-            { !imageLoading ? <Image 
-            width={400}
-            height={400}
-            src='/trail-5yOnGsKUNGw-unsplash.jpg'
-            onLoad={()=> { setImageLoading(true) }}
-            onLoadingComplete={()=> { setImageLoading(false) }}
-            />  : 
-            <Image 
-            width={400}
-            height={400}
-            src='/trail-5yOnGsKUNGw-unsplash_11zon.jpg'
-            layout="fill"
-            placeholder="blur"
-            priority
+        <section>
+          <h1> Using low quality Image as placeholder </h1>
+          <div
+            style={{ visibility: !imageLoading && "hidden" }}
+          >
+            <Image
+              width={400}
+              height={400}
+              src='/trail-5yOnGsKUNGw-unsplash.jpg'
+              onLoad={() => { setImageLoading(true) }}
             />
-            }
+          </div>
+          <div
+            style={{ visibility: imageLoading && "hidden" }}
+          >
+            <Image
+              width={400}
+              height={400}
+              src='/trail-5yOnGsKUNGw-unsplash_11zon.jpg'
+              priority
+            />
+          </div>
 
-          </section>
+        </section>
       </main>
     </div>
   );
